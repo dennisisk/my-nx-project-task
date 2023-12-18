c80b811aba70f03647de6c08cd95a94f1d92093a
dennisisk <97694680+dennisisk@users.noreply.github.com>
2023-12-17 20:23:00 -0500

    add step 8

---
+++ apps/my-app/component-b/component-b.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+// eslint-disable-next-line @nx/enforce-module-boundaries
+import { ApiServiceService } from '../../../libs/my-library/src/lib/api-service.service';
+
+@Component({
+  selector: 'my-nx-project-component-b',
+  templateUrl: './component-b.component.html',
+  styleUrls: ['./component-b.component.css'],
+})
+export class ComponentBComponent {}
--- libs/my-library/src/lib/api-service.service.ts
+++ libs/my-library/src/lib/api-service.service.ts
@@ -1,14 +1,20 @@
 import { Injectable, InjectionToken, Inject } from '@angular/core';
+import { of } from 'rxjs';
+import { delay } from 'rxjs/operators';
 
 export const API_TOKEN = new InjectionToken<string>('API_TOKEN');
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ApiServiceService {
   constructor(@Inject(API_TOKEN) private apiValue: string) {}
 
   test() {
     console.log('ApiService test()', 'API Value:', this.apiValue);
+  }
+
+  getData() {
+    return of('Data from API').pipe(delay(1000));
   }
 }
