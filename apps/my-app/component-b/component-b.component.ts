e75801edb2552fafbaf940675224385baaf572c0
dennisisk <97694680+dennisisk@users.noreply.github.com>
2023-12-18 09:09:46 -0500

    Update component-b.component.ts

--- apps/my-app/component-b/component-b.component.ts
+++ apps/my-app/component-b/component-b.component.ts
@@ -7,4 +7,12 @@ import { ApiServiceService } from '../../../libs/my-library/src/lib/api-service.
   templateUrl: './component-b.component.html',
   styleUrls: ['./component-b.component.css'],
 })
-export class ComponentBComponent {}
+export class ComponentBComponent {
+  data$ = this.apiService.getData();
+
+  constructor(private apiService: ApiServiceService) {}
+
+  ngOnInit() {
+    this.data$.subscribe((data) => console.log('Component A Data:', data));
+  }
+}
