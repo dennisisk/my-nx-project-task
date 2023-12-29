1dc39ca96647174b17569ee549f01c0081a710de
Dennis Tou <153868879+ai-dennis-tou@users.noreply.github.com>
2023-12-29 11:48:55 -0500

    Update app.component.ts

--- apps/my-app/src/app/app.component.ts
+++ apps/my-app/src/app/app.component.ts
@@ -1,8 +1,13 @@
+/* eslint-disable @nx/enforce-module-boundaries */
 import { Component } from '@angular/core';
-// eslint-disable-next-line @nx/enforce-module-boundaries
 import { ApiServiceService } from 'libs/my-library/src/lib/api-service.service';
 import { AppStateService } from './app-state.service';
-
+import { MyAuthGuard } from './../../../../libs/my-library/src/lib/my-auth.guard';
+import { HttpClient } from '@angular/common/http';
+import { MyHttpInterceptor } from './../../../../libs/my-library/src/lib/my-http-interceptor';
+import { MyErrorHandler } from './../../../../libs/my-library/src/lib/my-error-handler';
+import { myBootstrapListener } from './../../../../libs/my-library/src/lib/my-bootstrap-listener';
+import { AppInitializerService } from './../../../../libs/my-library/src/lib/my-app-initializer';
 @Component({
   selector: 'my-nx-project-root',
   template: `
@@ -19,9 +24,32 @@ export class AppComponent {
 
   constructor(
     private apiService: ApiServiceService,
-    private state: AppStateService
-  ) {
+    private state: AppStateService,
+    private http: HttpClient,
+    private myAuthGuard: MyAuthGuard,
+    private myHttpInterceptor: MyHttpInterceptor,
+    private myErrorHandler: MyErrorHandler,
+    private appInitializerService: AppInitializerService
+      ) {
     this.apiService.test();
+  }
+
+  ngOnInit() {
+    this.http.get('https://api.example.com/data').subscribe((data) => {
+      console.log('Data received:', data);
+    });
+
+    try {
+      console.log('error');
+    } catch (error) {
+      console.error('An error occurred:', error);
+    }
+
+    this.appInitializerService.initializeApp().then(() => {
+      console.log('App initialized');
+    });
+
+    myBootstrapListener();
   }
 
   increment() {
